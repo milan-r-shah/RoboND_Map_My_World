3656bae96f6173489038d50408456e30a3c7c424
Milan Shah <mrshah@wpi.edu>
2019-07-06 23:57:10 -0400

    Updated process_image.cpp, CMakeLists.txt and ball_chaser.launch.

--- catkin_ws/src/ball_chaser/src/process_image.cpp
+++ catkin_ws/src/ball_chaser/src/process_image.cpp
@@ -9,6 +9,11 @@ ros::ServiceClient client;
 void drive_robot(float lin_x, float ang_z)
 {
     // TODO: Request a service and pass the velocities to it to drive the robot
+    ball_chaser::DriveToTarget srv;
+    srv.request.linear_x = lin_x;
+    srv.request.angular_z = ang_z;
+
+    client.call(srv);
 }
 
 // This callback function continuously executes and reads the image data
@@ -16,11 +21,60 @@ void process_image_callback(const sensor_msgs::Image img)
 {
 
     int white_pixel = 255;
+    int white_pixel_cnt = 0;
+    int white_pixel_cnt_threshold = 0;
 
     // TODO: Loop through each pixel in the image and check if there's a bright white one
     // Then, identify if this pixel falls in the left, mid, or right side of the image
     // Depending on the white ball position, call the drive_bot function and pass velocities to it
     // Request a stop when there's no white ball seen by the camera
+    for(int i = 0; i < img.height*img.step; i++)
+    {
+        if(img.data[i]==white_pixel)
+        {
+            white_pixel_cnt++;
+        }
+    }
+    if(white_pixel_cnt > white_pixel_cnt_threshold)
+    {
+        int white_pixel_left_cnt = 0;
+        int white_pixel_forward_cnt = 0;
+        int white_pixel_right_cnt = 0;
+
+        for(int h=0; h<img.height; h++)
+        {
+            for(int s=0; s<0.3*img.step; s++)
+            {
+                white_pixel_left_cnt++;
+            }
+            for(int s=0.3*img.step; s < 0.7*img.step; s++)
+            {
+                white_pixel_forward_cnt++;
+            }
+            for(int s=0.7*img.step; s < img.step; s++)
+            {
+                white_pixel_right_cnt++;
+            }
+        }
+
+        if((white_pixel_left_cnt > white_pixel_right_cnt) && (white_pixel_left_cnt > white_pixel_forward_cnt))
+        {
+            drive_robot(0.5, -0.5);
+        }
+        else if((white_pixel_forward_cnt > white_pixel_left_cnt) && (white_pixel_forward_cnt > white_pixel_right_cnt))
+        {
+            drive_robot(0.5, 0.0);
+        }
+        else
+        {
+            drive_robot(0.5, +0.5);
+        }
+        
+    }
+    else
+    {
+        drive_robot(0.0, 0.0);
+    }
 }
 
 int main(int argc, char** argv)
