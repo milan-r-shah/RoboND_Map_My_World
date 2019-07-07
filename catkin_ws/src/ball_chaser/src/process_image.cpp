cd20d996c5c395b9533bcd3eda0214ed329dbacd
Milan Shah <mrshah@wpi.edu>
2019-07-07 02:22:34 -0400

    Updated the 'process_image.cpp' file so that robot chases white ball.

--- catkin_ws/src/ball_chaser/src/process_image.cpp
+++ catkin_ws/src/ball_chaser/src/process_image.cpp
@@ -8,7 +8,7 @@ ros::ServiceClient client;
 // This function calls the command_robot service to drive the robot in the specified direction
 void drive_robot(float lin_x, float ang_z)
 {
-    // TODO: Request a service and pass the velocities to it to drive the robot
+    // Request a service and pass the velocities to it to drive the robot
     ball_chaser::DriveToTarget srv;
     srv.request.linear_x = lin_x;
     srv.request.angular_z = ang_z;
@@ -19,12 +19,11 @@ void drive_robot(float lin_x, float ang_z)
 // This callback function continuously executes and reads the image data
 void process_image_callback(const sensor_msgs::Image img)
 {
-
     int white_pixel = 255;
     int white_pixel_cnt = 0;
     int white_pixel_cnt_threshold = 0;
 
-    // TODO: Loop through each pixel in the image and check if there's a bright white one
+    // Loop through each pixel in the image and check if there's a bright white one
     // Then, identify if this pixel falls in the left, mid, or right side of the image
     // Depending on the white ball position, call the drive_bot function and pass velocities to it
     // Request a stop when there's no white ball seen by the camera
@@ -35,41 +34,56 @@ void process_image_callback(const sensor_msgs::Image img)
             white_pixel_cnt++;
         }
     }
+
+    // Number of white pixels in the left, middle, and right part of the image, respectively
+    int left_white_pixel_cnt = 0;
+    int middle_white_pixel_cnt = 0;
+    int right_white_pixel_cnt = 0;
+
     if(white_pixel_cnt > white_pixel_cnt_threshold)
     {
-        int white_pixel_left_cnt = 0;
-        int white_pixel_forward_cnt = 0;
-        int white_pixel_right_cnt = 0;
-
         for(int h=0; h<img.height; h++)
         {
             for(int s=0; s<0.3*img.step; s++)
             {
-                white_pixel_left_cnt++;
+                if(img.data[s + img.step*h]==white_pixel)
+                {
+                    left_white_pixel_cnt++;
+                }
             }
             for(int s=0.3*img.step; s < 0.7*img.step; s++)
             {
-                white_pixel_forward_cnt++;
+                if(img.data[s + img.step*h]==white_pixel)
+                {
+                    middle_white_pixel_cnt++;
+                }
             }
             for(int s=0.7*img.step; s < img.step; s++)
             {
-                white_pixel_right_cnt++;
+                if(img.data[s + img.step*h]==white_pixel)
+                {
+                    right_white_pixel_cnt++;
+                }
             }
         }
 
-        if((white_pixel_left_cnt > white_pixel_right_cnt) && (white_pixel_left_cnt > white_pixel_forward_cnt))
+        // If the white ball is in the left side of the image, turn left
+        if((left_white_pixel_cnt > right_white_pixel_cnt) && (left_white_pixel_cnt > middle_white_pixel_cnt))
         {
-            drive_robot(0.5, -0.5);
+            drive_robot(0.3, 0.3);
         }
-        else if((white_pixel_forward_cnt > white_pixel_left_cnt) && (white_pixel_forward_cnt > white_pixel_right_cnt))
+
+        // If the white ball is in the middle part of the image, go straight
+        else if((middle_white_pixel_cnt > left_white_pixel_cnt) && (middle_white_pixel_cnt > right_white_pixel_cnt))
         {
-            drive_robot(0.5, 0.0);
+            drive_robot(0.3, 0.0);
         }
+
+        // If the white ball is in the right side of the image, turn right
         else
         {
-            drive_robot(0.5, +0.5);
+            drive_robot(0.3, -0.3);
         }
-        
     }
     else
     {
