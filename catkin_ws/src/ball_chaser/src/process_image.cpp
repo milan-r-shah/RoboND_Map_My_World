5c9f58ba2a49d994a1a452294af8a82f1a4dfb1f
Milan Shah <mrshah@wpi.edu>
2019-07-07 21:27:42 -0400

    Optimized the 'process_image.cpp' code.
    Initially, I was first checking that in the image, whether there is
    a white ball or not. Then I was deciding it is in which part. But then
    I found that it's unnecessary. So, now I'm checking ball is in which
    part. If it's in none of the parts of an image which means that it is
    not in the image.

--- catkin_ws/src/ball_chaser/src/process_image.cpp
+++ catkin_ws/src/ball_chaser/src/process_image.cpp
@@ -19,61 +19,62 @@ void drive_robot(float lin_x, float ang_z)
 // This callback function continuously executes and reads the image data
 void process_image_callback(const sensor_msgs::Image img)
 {
-    ROS_INFO(" = = = = = = = = = = = = = = = = = = = = = = = = = =");
-    ROS_INFO_STREAM(img.height);
-    ROS_INFO_STREAM(img.width);
-    ROS_INFO_STREAM(img.step);
     int white_pixel = 255;
-    // ROS_INFO("white pixel value: %d", white_pixel);
-    ROS_INFO(" = = = = = = = = = = = = = = = = = = = = = = = = = =");
-    int white_pixel_cnt = 0;
-    int white_pixel_cnt_threshold = 0;
 
     // Loop through each pixel in the image and check if there's a bright white one
     // Then, identify if this pixel falls in the left, mid, or right side of the image
     // Depending on the white ball position, call the drive_bot function and pass velocities to it
     // Request a stop when there's no white ball seen by the camera
-    for(int i = 0; i < img.height*img.step; i = i + 3)
-    {
-        if((img.data[i]==white_pixel) && (img.data[i+1]==white_pixel) && (img.data[i+2]==white_pixel))
-        {
-            white_pixel_cnt++;
-        }
-    }
 
     // Number of white pixels in the left, middle, and right part of the image, respectively
     int left_white_pixel_cnt = 0;
     int middle_white_pixel_cnt = 0;
     int right_white_pixel_cnt = 0;
 
-    if(white_pixel_cnt > white_pixel_cnt_threshold)
+    for(int h=0; h<img.height; h++)
     {
-        for(int h=0; h<img.height; h++)
+        // Dividing the image width or to be precise its 'step' into three parts by 30% left, next 40% into middle, & remaining 30% into right
+        
+        // Left part/side
+        for(int s=0; s<0.3*img.step; s = s + 3)
         {
-            for(int s=0; s<0.3*img.step; s = s + 3)
+            // In 'sensor_msgs/image', data is arranged in a 1D vector whereby one pixel is represented by three consecutive bytes (uint8) comprising the RED, BLUE, and GREEN color information.
+            // So, in each iteration, I'm checking three consecutive pixels/bytes
+            if((img.data[s + img.step*h] == white_pixel) && (img.data[s + img.step*h + 1] == white_pixel) && (img.data[s + img.step*h+ 2] == white_pixel))
             {
-                if((img.data[s + img.step*h] == white_pixel) && (img.data[s + img.step*h + 1] == white_pixel) && (img.data[s + img.step*h+ 2] == white_pixel))
-                {
-                    left_white_pixel_cnt++;
-                }
-            }
-            for(int s=0.3*img.step; s < 0.7*img.step; s = s + 3)
-            {
-                if((img.data[s + img.step*h] == white_pixel) && (img.data[s + img.step*h + 1] == white_pixel) &&(img.data[s + img.step*h+ 2] == white_pixel))
-                {
-                    middle_white_pixel_cnt++;
-                }
-            }
-            for(int s=0.7*img.step; s < img.step; s = s + 3)
-            {
-                if((img.data[s + img.step*h] == white_pixel) && (img.data[s + img.step*h + 1] == white_pixel) && (img.data[s + img.step*h+ 2] == white_pixel))
-                {
-                    right_white_pixel_cnt++;
-                }
+                left_white_pixel_cnt++;
             }
         }
 
-        // If the white ball is in the left side of the image, turn left
+        // middle part
+        for(int s=0.3*img.step; s < 0.7*img.step; s = s + 3)
+        {
+            if((img.data[s + img.step*h] == white_pixel) && (img.data[s + img.step*h + 1] == white_pixel) &&(img.data[s + img.step*h+ 2] == white_pixel))
+            {
+                middle_white_pixel_cnt++;
+            }
+        }
+
+        // right part/side
+        for(int s=0.7*img.step; s < img.step; s = s + 3)
+        {
+            if((img.data[s + img.step*h] == white_pixel) && (img.data[s + img.step*h + 1] == white_pixel) && (img.data[s + img.step*h+ 2] == white_pixel))
+            {
+                right_white_pixel_cnt++;
+            }
+        }
+    }
+
+    // First, check if there is a white ball in the image or not
+    // If not then stop 
+    if((left_white_pixel_cnt == 0) && (middle_white_pixel_cnt == 0) && (right_white_pixel_cnt == 0))
+    {
+        drive_robot(0.0, 0.0);
+    }
+    // if it is then check it's in which part of the image
+    else
+    {
+        // If the white ball is in the left side of the image, turn right
         if((left_white_pixel_cnt > right_white_pixel_cnt) && (left_white_pixel_cnt > middle_white_pixel_cnt))
         {
             drive_robot(0.3, 0.3);
@@ -90,10 +91,6 @@ void process_image_callback(const sensor_msgs::Image img)
         {
             drive_robot(0.3, -0.3);
         }
-    }
-    else
-    {
-        drive_robot(0.0, 0.0);
     }
 }
 
